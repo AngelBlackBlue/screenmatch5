e2ce027634dc5e6352d8a0573959e1e42f75ec2f
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-17 16:06:27 -0300

    update

--- src/main/java/com/aluracursos/screenmatch5/Screenmatch5Application.java
+++ src/main/java/com/aluracursos/screenmatch5/Screenmatch5Application.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmatch5;
 
+import com.aluracursos.screenmatch5.model.DatosSeries;
 import com.aluracursos.screenmatch5.service.ConsumoAPI;
+import com.aluracursos.screenmatch5.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +19,9 @@ public class Screenmatch5Application implements CommandLineRunner {
 		var consumoApi = new ConsumoAPI();
 		var json = consumoApi.obtenerDatos("https://www.episodate.com/api/show-details?q=46778");
 		System.out.println(json);
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSeries.class);
+		System.out.println(datos);
 
 	}
 }
--- src/main/java/com/aluracursos/screenmatch5/model/DatosSeries.java
+++ src/main/java/com/aluracursos/screenmatch5/model/DatosSeries.java
@@ -1,7 +1,9 @@
 package com.aluracursos.screenmatch5.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosSeries(
         @JsonAlias("name") String titulo,
         @JsonAlias("description") String description,
