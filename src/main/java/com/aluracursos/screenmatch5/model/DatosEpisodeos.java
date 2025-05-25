f4c5eed871c946d0b86b177e15e8deed54c6187b
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-25 20:39:05 -0300

    Create DatosEpisodeos.java

---
+++ src/main/java/com/aluracursos/screenmatch5/model/DatosEpisodeos.java
@@ -0,0 +1,12 @@
+package com.aluracursos.screenmatch5.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosEpisodeos(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("Episode") Integer numeroEpisodeo,
+        @JsonAlias("imdbRating") String evaluacion,
+        @JsonAlias("Released") String fechaDeLanzamiento) {
+}
