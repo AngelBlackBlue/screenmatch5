b6c3ef622bfdfa11171f65659215fe23262d07b9
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-25 20:19:07 -0300

    Update DatosSeries.java

--- src/main/java/com/aluracursos/screenmatch5/model/DatosSeries.java
+++ src/main/java/com/aluracursos/screenmatch5/model/DatosSeries.java
@@ -5,13 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosSeries(
-        @JsonAlias("tvShow") TvShow tvShow
-) {
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public record TvShow(
-            @JsonAlias("name") String titulo,
-            @JsonAlias("description") String description,
-            @JsonAlias("start_date") String start,
-            @JsonAlias("rating") String rating
-    ) {}
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons")Integer totalTemporadas,
+        @JsonAlias("imdbRating") String evaluacion) {
 }
