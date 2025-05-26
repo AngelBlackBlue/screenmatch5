447fd6b35c17eddfe5b05908250d8232686affcb
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-26 09:03:00 -0300

    Update Screenmatch5Application.java

--- src/main/java/com/aluracursos/screenmatch5/Screenmatch5Application.java
+++ src/main/java/com/aluracursos/screenmatch5/Screenmatch5Application.java
@@ -2,11 +2,15 @@ package com.aluracursos.screenmatch5;
 
 import com.aluracursos.screenmatch5.model.DatosEpisodio;
 import com.aluracursos.screenmatch5.model.DatosSeries;
+import com.aluracursos.screenmatch5.model.DatosTemporada;
 import com.aluracursos.screenmatch5.service.ConsumoAPI;
 import com.aluracursos.screenmatch5.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Screenmatch5Application implements CommandLineRunner {
@@ -28,6 +32,17 @@ public class Screenmatch5Application implements CommandLineRunner {
 		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Stranger+Things&Season=1&episode=1&&apikey=812f93ca");
 		DatosEpisodio episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
 		System.out.println(episodio);
+
+		List<DatosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i < datos.totalTemporadas() ; i++) {
+			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Stranger+Things&Season="+i+"&&apikey=812f93ca");
+			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporada.class);
+			temporadas.add(datosTemporadas);
+		}
+
+		temporadas.forEach(System.out::println);
+
 
 
 	}
